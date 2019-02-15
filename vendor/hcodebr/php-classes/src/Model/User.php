94ae35bb7e08267e07ee4da5d2fe20881c23b39b
wiltonabreu <wiltonabreuu@gmail.com>
2019-02-15 15:00:51 -0200

    trabalhando na class model

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -3,8 +3,9 @@
 namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
+use \Hcode\Model;
 
-class User
+class User extends Model
 {
     public function login($login, $password){
 
@@ -24,6 +25,10 @@ class User
 
         if(password_verify($password,$data["despassword"]) === true){
             $user = new User();
+
+            $user->setData($data);
+
+            return $user;
         }else {
             throw new \Exception("Usuário inexistente ou senha inválida");
         }
